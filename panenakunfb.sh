c7fe23cbbfd898f9a42e428f8826543cd5606b9b
aimscar <73692881+aimscar@users.noreply.github.com>
2020-11-04 12:23:16 +0700

    Add files via upload

---
+++ panenakunfb.sh
@@ -0,0 +1,53 @@
+
+clear
+                                                            
+figlet BACA | lolcat
+sleep 1
+figlet DULU | lolcat
+sleep 1
+figlet Wooii | lolcat
+sleep 2
+clear
+sleep 3
+echo "================================"
+sleep 1
+echo "script milik=AIMSCAR,HANIEF,IM IT"
+sleep 2
+echo "chanel yt =AIMSCAR && IM IT HACKING"
+sleep 2
+echo "DAN SUBSCRIBE CHANEL GURU IT SAYA  "
+sleep 2
+echo "Hanief Aktivisme"                               
+sleep 2
+echo "================================"
+sleep 8
+figlet -f small "Ngentot yuk"
+echo "(1)Hack Fb Login Via Cookie"
+sleep 1
+echo "(2)Install bahan dulu cok"
+sleep 1
+echo "(3)Exit"
+sleep 1
+read -p "pilih nomor: " input;
+if [ $input = 1 ]
+then
+clear
+git clone https://github.com/haniefaktivisme/fbcrack.git
+cd fbcrack
+ls
+python crack.py
+cowok
+fi
+if [ $input = 2 ]
+then
+clear
+pkg update;pkg upgrade;pkg install python2;pkg install git;pip2 install requests;pip2 install mechanize;git clone https://github.com/aimscar/panenakunfb.sh.git;ls;cd panenakunfb.sh;ls;bash panenakunfb.sh;cowok;Tripel666
+fi
+if [ $input = 3 ]
+then
+clear
+echo " tunggu sebentar lagi coli...."
+sleep 3
+exit
+fi
+
